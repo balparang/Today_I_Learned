2003d6d6159cb416fe01c5ea9edef8c63eb52f97
haero77 <shineecard5@gmail.com>
2025-02-01 11:28:41 +0900

    [Spring-Security](Securing Web) initial commit

---
+++ Spring/projects/spring-security/gs-securing-web/src/main/java/com/example/gssecuringweb/GsSecuringWebApplication.java
@@ -0,0 +1,12 @@
+package com.example.gssecuringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsSecuringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsSecuringWebApplication.class, args);
+    }
+}
---
+++ Spring/projects/spring-security/gs-securing-web/src/test/java/com/example/gssecuringweb/GsSecuringWebApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.gssecuringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsSecuringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
